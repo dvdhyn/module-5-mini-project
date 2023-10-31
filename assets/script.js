4d349dddd55e6cb8c191e3ee60f59ba2492ce626
yungtaco <dvhyun@gmail.com>
2023-10-30 21:21:04 -0400

    Fixed time setInterval issue

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,9 @@
 var currentTime=document.querySelector("#currentTime");
 
+currentTime.innerText=dayjs().format("dddd MM/DD/YYYY hh:mm:ss");
+
 setInterval(function(){
     currentTime.innerText=dayjs().format("dddd MM/DD/YYYY hh:mm:ss");
 },1000);
+
 
